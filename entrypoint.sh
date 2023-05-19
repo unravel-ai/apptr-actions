f2b7a480d92cd88ac9084ea2096342b8fab8b7ed
anuj-kosambi <anujkosambi@gmail.com>
2023-05-19 15:09:42 +0530

    fix: github actions cache for bazel

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ mkdir -p $(dirname $1)
 touch $1
 echo "Generating output file: "$(ls $1)
 
-mkdir -p .cache/bazel
-echo "build --disk_cache=.cache/bazel" >> .bazelrc
-
 skaffold build --file-output="$1" \
   --default-repo="$2" --tag="$GITHUB_SHA"
 
