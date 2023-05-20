f20f8fda374f861420b7b47ee5bbf7e300086c5b
anuj-kosambi <anujkosambi@gmail.com>
2023-05-20 17:39:28 +0530

    fix: removing cache dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ mkdir -p $(dirname $1)
 touch $1
 echo "Generating output file: "$(ls $1)
 
-mkdir -p ~/.cache/bazel
+# mkdir -p ~/.cache/bazel
 
-chown $(id -g):$(id -u) -R ~/.cache
+# chown $(id -g):$(id -u) -R ~/.cache
 
 skaffold build --file-output="$1" \
   --default-repo="$2" --tag="$GITHUB_SHA"
 
-chown 1001:1001 -R ~/.cache/bazel
+# chown 1001:1001 -R ~/.cache/bazel
