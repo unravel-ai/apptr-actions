9f8239e4f0d8c86a4cb7ca653adfe2977fb26f5a
anuj-kosambi <anujkosambi@gmail.com>
2023-05-22 19:28:52 +0530

    fix: remove qoute from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,8 @@ echo "Generating output file: "$(ls $1)
 # mkdir -p ~/.cache/bazel
 
 mkdir -p /tmp/bazel_s3_cache 
-bazel-remote -dir=/tmp/bazel_s3_cache --s3.region=us-east-2 --s3.bucket=apptr-ci-cd-cache --s3.access_key_id='$BAZEL_REMOTE_CACHE_S3_ID' --s3.secret_access_key='$BAZEL_REMOTE_CACHE_S3_KEY' --max_size=5 --s3.auth_method=access_key --s3.endpoint=s3.us-east-2.amazonaws.com --port=9090 &
+
+bazel-remote -dir=/tmp/bazel_s3_cache --s3.region=us-east-2 --s3.bucket=apptr-ci-cd-cache --s3.access_key_id=$BAZEL_REMOTE_CACHE_S3_ID --s3.secret_access_key=$BAZEL_REMOTE_CACHE_S3_KEY --max_size=5 --s3.auth_method=access_key --s3.endpoint=s3.us-east-2.amazonaws.com --port=9090 &
 BAZEL_REMOTE_PID=$!
 # chown $(id -g):$(id -u) -R ~/.cache
 
